2e50b9834f752ae90f8d3a9d18bdbb239ed153d3
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-03 11:21:51 -0800

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Lister::Application.routes.draw do
   
+  get '/login' => 'session#new'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
