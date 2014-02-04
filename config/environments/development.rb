788dfbb983c386c58459eda979f20e066f913561
liz rush <liz@lizmrush.com>
2014-02-04 10:48:01 -0800

    adds figaro and resque

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,17 @@ Lister::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.mandrillapp.com",
+    port:                 587,
+    enable_starttls_auto: true,
+    user_name:            "liz.m.rush@gmail.com", # YOUR MANDRILL USERNAME
+    password:             ENV["MANDRILL_KEY"], # A MANDRILL API KEY
+    authentication:       'login',
+    domain:               'yourdomain.com'
+  }
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
