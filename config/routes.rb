b1cd9fa32d054baf0e7da07f24b18c257c5ed197
Audrey Carlsen <audreycarlsen@gmail.com>
2014-02-04 13:06:25 -0800

    Created items controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Lister::Application.routes.draw do
   
   root 'session#new'
 
-  get 'session/new'     => 'session#new',    as: 'new_session'
-  post 'session/create' => 'session#create', as: 'create_session'
-  get 'users/new'       => 'users#new',      as: 'new_user'
-  post 'users/create'   => 'users#create',   as: 'create_user'
-  get 'users/show'      => 'users#show',     as: 'show_user'
+  get 'session/new'      => 'session#new',    as: 'new_session'
+  post 'session/create'  => 'session#create', as: 'create_session'
+
+  get 'users/new'        => 'users#new',      as: 'new_user'
+  post 'users/create'    => 'users#create',   as: 'create_user'
+  get 'users/show'       => 'users#show',     as: 'show_user'
+
+  post 'items/new'       => 'items#new',      as: 'new_item'
+  patch 'items/update'   => 'items#update',   as: 'update_item'
+  delete 'items/destroy' => 'items#destroy',  as: 'destroy_item'
 
   resources :lists
 
---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe ItemsController do
+
+  let(:item) { create(:item) }
+
+  describe "POST 'create'" do
+  end
+
+  describe "PATCH 'update'" do
+  end
+
+  describe "DELETE 'destroy'" do
+  end
+
+end
