6420d788790249d63a22ec4dc6a9d0a9a2e86818
Galina <yermichevagalina@gmail.com>
2014-02-03 16:33:49 -0800

    specs are passing

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,7 @@ class SessionController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    if user && user.authenticate[params[:password]]
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Successfully signed in'
     else
