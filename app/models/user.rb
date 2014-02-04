7299d096a58a330ee819035777650365e6ed1b00
Galina <yermichevagalina@gmail.com>
2014-02-03 16:22:40 -0800

    session specs aren't passing

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email
+  validates :password, length: { minimum: 6 , message: "must be at least 6 characters" }
+  validates :email, format: { with: /@/, message: "is not a valid address" }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Lister::Application.routes.draw do
-  
+
+
+  root 'users#index'
+  resources :users
+  resources :session
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
