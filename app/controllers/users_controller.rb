6dc042189a7d14234c8417762ae07a9d81d660b8
liz rush <liz@lizmrush.com>
2014-02-05 12:54:21 -0800

    made new user a form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,30 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
+    @lists = List.all
+    @users_lists = List.where(user_id: @current_user.id) if @current_user
   end
 
   def show
+    @users = User.all
   end
 
   def  new
+    @user = User.new
+
   end
 
   def create
     @user = User.new(user_params)
+    respond_to do |format|
     if @user.save
       Resque.enqueue(EmailJob, @user.id)
         redirect_to root_path, notice: "Successfully created new user"
       else
         render :new
       end
+    end
   end
 
   def edit
