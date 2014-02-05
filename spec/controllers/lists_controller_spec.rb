d88bfdfefb92e7f2b0696761684e3dc4366d8dc3
Christina Thompson <c.thompsonms@gmail.com>
2014-02-05 12:00:07 -0800

    created spec to test list assignment

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -1,13 +1,26 @@
 require 'spec_helper'
 
 describe ListsController do
+
     describe "GET 'new'" do
   
       it "is successful" do
         get :new
         expect(response).to be_successful
       end
-
-      
     end
+
+    describe "POST 'create'" do 
+
+      it "assigns a list instance to @list" do
+        post :create, list: {list_name: "to_do"}
+        expect(assigns(:list)).to be_a_kind_of(List)
+      end
+
+      it "should redirect" do
+        post :create, list: {list_name: "to_do"}
+        expect(response).to redirect_to(list_path(assigns(:list).id))
+      end
+    end
+    
 end
