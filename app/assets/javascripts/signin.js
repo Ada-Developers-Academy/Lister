13e483a98516aa699e2d30cd4e2b57d98d972081
liz rush <liz@lizmrush.com>
2014-02-05 15:37:21 -0800

    lists all lists

--- app/assets/javascripts/signin.js
+++ app/assets/javascripts/signin.js
@@ -2,6 +2,7 @@ $(function() {
     $('#sign-in-area').hide();
 
   $(".signin").click(function(e){
+    console.log("yeash")
     $('#sign-up-area').hide();
     $('#sign-in-area').toggle();
     e.preventDefault();
