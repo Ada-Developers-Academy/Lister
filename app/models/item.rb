229d06e8e32bc0c2a2c7ba616fc0f04eafab61b0
liz rush <liz@lizmrush.com>
2014-02-06 15:26:22 -0800

    steals elizabeths sweet uniquness in scope validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
   validates :name, :list_id, presence: true
+  validates :name, uniqueness: { scope: :list, message: "you've already got that item" }
   belongs_to :list
 end
