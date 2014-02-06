9749c9b0bebda16b5b344b098b5e0238aae6af84
Anne Kimsey <anne.kimsey@gmail.com>
2014-02-06 14:43:04 -0800

    added bootstrap and messed with the sessions controller a bit.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,7 @@ class SessionController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
+      @current_user = @user
       redirect_to '/', notice: 'Successfully signed in'
     else
       flash[:notice] = "Invalid username or password"
