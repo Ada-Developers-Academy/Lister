c1801ecf18a78c0d5f980a1d45b8e0817f27a634
Anne Kimsey <anne.kimsey@gmail.com>
2014-02-06 14:17:01 -0800

    List form works

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,11 +4,16 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(params.require(:list).permit(:title, :user_id))
-    if @list.save
-      redirect_to '/', notice: "Thanks for listing your list!"
+    unless session[:user_id].nil?
+      @list = List.new(params.require(:list).permit(:title, :user_id))
+      @list.user_id = session[:user_id]
+      if @list.save
+        redirect_to lists_path(@list.id), notice: "Thanks for listing your list!" 
+      else
+        render :new, notice: "Something went wrong :("
+      end
     else
-      render :new, notice: "Something went wrong :("
+      redirect_to sign_in_path, notice: "You need to sign in to list a list!"
     end
   end
 
