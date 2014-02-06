f447acd6399209a7b27d208c3c2b252d41ad731b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-06 10:49:13 -0800

    start specs on list model

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-# describe List do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe List do
+  it "has no lists in the database" do
+    expect(List).to have(:no).records
+    expect(List).to have(0).records
+  end
+
+  it "has one record" do
+    create(:list)
+    expect(List).to have(1).record
+  end
+end
