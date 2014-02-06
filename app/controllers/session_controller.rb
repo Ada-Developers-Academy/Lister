020a1966eeaeef3126151a8f527d7a05df2a7c8b
Christina Thompson <c.thompsonms@gmail.com>
2014-02-06 12:19:00 -0800

    user signin

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,21 +1,24 @@
 class SessionController < ApplicationController
   
   def new
+    @user = User.new
   end
 
   def create
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && @user.authenticate(params[:user][:password])
       sign_in
-      redirect_to "/", notice: "Successfully signed in"
+      redirect_to user_path(@user.id), notice: "Successfully signed in"
     else
+      @user = User.new
       flash[:notice] = "Invalid username or password"
       render "new"
     end
   end
 
   def sign_in
-    session[:user_id] = user.id
+    session[:user_id] = @user.id
   end
 
   def sign_out
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Lister::Application.routes.draw do
 
   root to: "welcome#index"
   get "/session/new", to: 'session#new'
-  post 'session', to: 'session#create'
+  post 'session', to: 'session#create', as: :signin
   get '/users/new', to: 'users#new', as: :signup
   post '/users', to: 'users#create', as: :create_user
   get '/users/show/:id', to: 'users#show', as: :user 
