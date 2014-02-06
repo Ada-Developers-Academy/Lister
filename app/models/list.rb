3710fb211e72dd5f7cb48fad17edfbd3a0aae7a4
Christina Thompson <c.thompsonms@gmail.com>
2014-02-06 10:56:04 -0800

    added validation message

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ActiveRecord::Base
   validates :list_name, presence: true
-  validates :list_name, length: {minimum: 1}
+  validates :list_name, length: {minimum: 1, message: "you must enter a list name with at least 1 character"}
   has_many :items
   belongs_to :user
 end
