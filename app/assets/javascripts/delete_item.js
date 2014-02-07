762fe5d20a32236625f840c14402ce942ff2ad7a
Audrey Carlsen <audreycarlsen@gmail.com>
2014-02-07 14:12:55 -0800

    added semicolons

--- app/assets/javascripts/delete_item.js
+++ app/assets/javascripts/delete_item.js
@@ -1,18 +1,17 @@
 jQuery(document).ready(function() {
   $(".items").on( "click", ".destroy", function() {
-    var item = $(this).parents('li')
+    var item = $(this).parents('li');
     $.ajax({
       url: $(this).attr("href"),
       type: 'DELETE',
       dataType: 'json',
       success: function(data, textStatus, xhr) {
-        console.log("dfda")
         item.remove();
       },
       error: function(xhr, textStatus, errorThrown) {
-        alert("There was a problem deleting this item.")
+        alert("There was a problem deleting this item.");
       }
     });
-    return false
+    return false;
   });
 });
