ec350f3d2fc20363e09a0b1f110e51a085060fd4
liz rush <liz@lizmrush.com>
2014-02-07 14:37:37 -0800

    jsut kidding, that td tag was right hte first time

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
   validates :name, :list_id, presence: true
-  validates :name, uniqueness: { scope: :list, message: "you've already got that item" }
   belongs_to :list
 end
