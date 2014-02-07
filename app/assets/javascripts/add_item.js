4627e75085e4caf4a56919c7f326380b61dbe463
Audrey Carlsen <audreycarlsen@gmail.com>
2014-02-07 13:21:43 -0800

    Adds item without refreshing, even the first item

--- app/assets/javascripts/add_item.js
+++ app/assets/javascripts/add_item.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   $(".add").click(function() {
 
     var items = $(".items");
-    
-    console.log( $("#item_name" ).val() );
+    var emptiness = $(".emptiness");
 
     $.ajax({
       url: $(this).parents('form').attr("action"),
@@ -16,8 +15,27 @@ $(document).ready(function() {
         }
       },
       success: function(data, textStatus, xhr) {
-        var delete_button = "<a class='btn btn-default btn-xs destroy' rel='nofollow' href='/items/" + data.id + " data-method='delete'>Delete</a>";
+        console.log("balh")
+        var delete_button = "<a class='btn btn-default btn-xs destroy' rel='nofollow' href='/items/" + data.id + "' data-method='delete'>Delete</a>";
         items.append("<li>"+ data.name + " " + delete_button + "</li>");
+        if(emptiness) 
+          emptiness.remove();
+
+        // var delete_button = $( "<a/>" );
+        // delete_button.addClass( "btn btn-default btn-xs destroy" );
+        // delete_button.attr( "rel", "nofollow" );
+        // delete_button.attr( "href", "/items/" + data.id );
+        // delete_button.attr( "data-method", "delete" );
+        // delete_button.text( "Delete" );
+
+        // var delete_button = document.createElement( "a" ); 
+        // delete_button.className = "btn btn-default btn-xs destroy";
+        // delete_button.rel = "nofollow";
+        // delete_button.href = "/items/" + data.id;
+        // delete_button.setAttribute( "data-method", "delete" );
+        // delete_button.innerHTML = "Delete";
+
+        // items.append("<li>"+ data.name + " " + delete_button.outerHTML + "</li>");
       },
       error: function(xhr, textStatus, errorThrown) {
         alert("Something has gone horribly wrong.");
