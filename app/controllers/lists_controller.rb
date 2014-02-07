f8a56fb31a6cde58e5844511f8737279f95eb50e
Anne Kimsey <anne.kimsey@gmail.com>
2014-02-07 11:06:35 -0800

    Code cleanup and you can now view a any user's user profile

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,7 +24,6 @@ class ListsController < ApplicationController
   def show
     @list = List.find_by(id: params[:id])
     @element = Element.new
-    @elements = Element.all
   end
 
   def destroy
