0ec8937be2f1d7a22db5cd823e9b728246a4c58a
Davida Marion <davidamarion@gmail.com>
2014-02-06 17:50:02 -0800

    ajax add works, needs to add the destroy

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".ajax-destroy").click(function(event) {
-   var item = $(this).parents('.item');
+   var removeItem = $(this).parents('li');
     $.ajax({
       url: $(this).attr("href"),
       type: 'DELETE',
@@ -8,7 +8,7 @@ $(document).ready(function() {
       complete: function(xhr, textStatus) {
       },
       success: function(data, textStatus, xhr) {
-        item.remove();
+        removeItem.remove();
       },
       error: function(xhr, textStatus, errorThrown) {
         alert("There was a problem deleting this item.");
@@ -16,4 +16,32 @@ $(document).ready(function() {
     });
     return false;
   });
+});
+
+
+$(document).ready(function() {
+  $('form#new_list_item').submit(function(event) {
+    event.preventDefault();
+    var valuesToSubmit = $('form').serializeArray();
+    console.log(valuesToSubmit)
+    $.ajax({
+      url: this.url,
+      type: 'POST',
+      data: valuesToSubmit,
+      error: function() {
+        alert('There was a problem adding this item.');
+      },
+      success: function() {
+        $('#items_list').each(function(){
+          console.log(valuesToSubmit[2])
+          var $li  = $("<li>").text(valuesToSubmit[2].value);
+          $(this).append($li);
+        });
+        $("input#list_item_description").val('');
+      },
+      complete: function() {
+      }
+    });
+  });
+  return false;
 });
--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -4,11 +4,12 @@ class ListItemsController < ApplicationController
 
   def create
     @list_item = ListItem.new(item_params)
-      if @list_item.save
-        redirect_to :back
-      else
-        redirect_to :back
+    if @list_item.save
+      respond_to do |format|
+        format.html { redirect_to list_path(params[:list_item][:list_id]) }
+        format.json { head :no_content}
       end
+    end
   end
 
   def destroy
@@ -17,6 +18,7 @@ class ListItemsController < ApplicationController
     @item.destroy
     respond_to do |format|
       format.json { head :no_content }
+      format.html { redirect_to list_path(@list.id)}
     end
   end
 
