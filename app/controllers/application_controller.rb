e99e8d1a7889ce432e4acda6b1143319f616d8b8
Christina Thompson <c.thompsonms@gmail.com>
2014-02-06 18:21:21 -0800

    make profile home for logged in users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,14 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
-  
+
+  def home_path
+    if current_user 
+      user_path
+    else
+      root_path
+    end
+  end
+  helper_method :home_path
+
 end
