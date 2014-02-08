6d2dfaeaff454a81215930b4135337d2b9e80e4d
Christina Thompson <c.thompsonms@gmail.com>
2014-02-07 16:15:50 -0800

    send email after creating a session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      ListMailer.welcome_email(@user.id).deliver
       redirect_to user_path(@user.id) , notice: "Successfully saved"
     else
       render :new
