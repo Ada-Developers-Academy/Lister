ff7a3ce514e5191fa6b36418b2bbdc1b54ff1058
Christina Thompson <c.thompsonms@gmail.com>
2014-02-07 16:14:12 -0800

    wrote method to send email to new users

--- app/mailers/list_mailer.rb
+++ app/mailers/list_mailer.rb
@@ -1,8 +1,8 @@
 class ListMailer < ActionMailer::Base
-  default from: "info@thelist.list"
+  default from: "info@thelist.com"
 
   def welcome_email(user_id)
     @user = User.find(user_id)
-    mail(to: "stalkmore00@gmail.com", subject: "Welcome to The List")
+    mail(to: @user.email, subject: "Welcome to The List")
   end
 end
