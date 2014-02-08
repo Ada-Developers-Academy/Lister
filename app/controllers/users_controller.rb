58bdf8ec6fb5d9aeca0d864477f0345f79c54c72
Christina Thompson <c.thompsonms@gmail.com>
2014-02-08 14:36:33 -0800

    actionmailler working but resque DOES NOT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      Resque.enqueue(EmailJob, @user.id)
+      ListMailer.welcome_email(@user.id).deliver
+      # Resque.enqueue(EmailJob, @user.id)
       redirect_to user_path(@user.id) , notice: "Successfully saved"
     else
       render :new
